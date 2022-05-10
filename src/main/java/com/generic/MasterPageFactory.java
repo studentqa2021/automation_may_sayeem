261ad5ef5be0fb3ee7d99654a010c9313b7fdf4f
studentqa2021 <sarowerny@gmail.com>
2022-05-10 18:45:42 -0400

    initial commit

---
+++ src/main/java/com/generic/MasterPageFactory.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 SAM
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.generic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class MasterPageFactory {
+	
+	public MasterPageFactory(WebDriver driver)
+	{
+		PageFactory.initElements(driver,this);
+	}
+	@FindBy(xpath = "//a[@class = 'login']")
+	private WebElement signIn;
+	@FindBy(xpath = "//*[@id = 'email']")
+	private WebElement username;
+	@FindBy(xpath = "//*[@id = 'passwd']")
+	private WebElement password;
+	@FindBy(xpath = "//*[@id = 'SubmitLogin']")
+	private WebElement submitLogin;
+	@FindBy(xpath = "//*[contains(text(),'Sign out')]")
+	private WebElement signOut;
+}
---
+++ src/test/java/com/practice/BaseLogin.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 SAM
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.practice;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import com.generic.CrossBrowserCheck;
+import com.generic.MasterPageFactory;
+import com.generic.DataProrertyReader;
+
+public class BaseLogin {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException, InterruptedException 
+	{
+		WebDriver driver = CrossBrowserCheck.crossBrowserCheck("chrome");
+		MasterPageFactory MPF = new MasterPageFactory(driver);
+		DataProrertyReader DPR = new DataProrertyReader();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(7,TimeUnit.SECONDS);
+		driver.get(DPR.getValue("url"));
+		MPF.getSignIn().click();
+		MPF.getUsername().sendKeys(DPR.getValue("email"));
+		MPF.getPassword().sendKeys(DPR.getValue("password"));
+		MPF.getSubmitLogin().click();
+		if(MPF.getSignOut().isDisplayed())
+		{
+			((JavascriptExecutor)driver).executeScript("alert('Login Successfully');");
+		}
+		Thread.sleep(5000);
+		driver.switchTo().alert().dismiss();
+	}
+
+}
